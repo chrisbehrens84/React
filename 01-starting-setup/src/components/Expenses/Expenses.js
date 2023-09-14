552411da048340485657b565ebd7a553717d9a35
chrisbehrens84 <cbehrens@skillstorm.com>
2023-09-14 13:08:15 -0700

    react project

---
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.expenses.map((expense, index) => {
+        return (
+          <ExpenseItem
+            key={index}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
+    </Card>
+  );
+}
+
+export default Expenses;
